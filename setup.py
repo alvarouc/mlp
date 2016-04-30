c1af4ce1c05c524cf259d0dd6fade7ae65e14b8f
aulloa <alvarouc@gmail.com>
2016-04-29 21:35:07 -0600

    added dependencies

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@ setup(
     name = 'mlp',
     packages = ['mlp'], # this must be the same as the name above
     version = '0.1',
+    install_requires=[
+        'keras',
+        'sklearn',
+        'numpy'],
     description = 'A multilayer perceptron implementation using keras and compatible with scikit-learn',
     author = 'Alvaro Ulloa',
     author_email = 'alvarouc@gmail.com',
     url = 'https://github.com/alvarouc/mlp', # use the URL to the github repo
-    download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+    download_url = 'https://github.com/alvarouc/mlp/tarball/0.1', # I'll explain this in a second
     keywords = ['neural net', 'mlp', 'deep learning'], # arbitrary keywords
     classifiers = [],
 )
